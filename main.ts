b4e4be32d18e049a1d909f1d874ec9dc6a31019e
saahil1111 <122548152+saahil1111@users.noreply.github.com>
2023-01-13 11:19:28 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Asleep)
+    basic.showString("why are you here?")
 })
-basic.showIcon(IconNames.Sad)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
